f51096ea20768eda84ca2049fbdec816e313d906
Juliana Torres <julianatc21@gmail.com>
2025-05-10 22:16:31 -0500

    First commit

---
+++ src_python/app.py
@@ -0,0 +1,11 @@
+# app.py
+from src_python.benchmarking import Benchmarking
+from grafica import plot_graph
+
+def main():
+    benchmarking = Benchmarking()
+    sizes, results = benchmarking.run_benchmark()
+    plot_graph(sizes, results)
+
+if __name__ == "__main__":
+    main()
